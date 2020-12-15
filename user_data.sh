b4bdf56c44a30a43e8db582f0b49d6f499013d2e
Artyom <artem96artem@gmail.com>
2020-12-15 14:00:23 +0300

    dobavlenit IP/poka ne rabotaet

--- user_data.sh
+++ user_data.sh
@@ -5,11 +5,15 @@ yum -y install httpd
 a=qwerty
 b=ytrewq!
 
+myip = `curl http://192.168.213.128/latest/meta-data/local-ipv4`
+
 cat <<EOF > /var/www/html/index.html
 <html>
 <body bgcolor="black">
 <h2><font color="gold">Hello!<h2></font>
 <h3><font color="red">$a<br>$b!<h3></font>
+
+<h2><font color="red">ServerIP: $myip </h2></font><br>
 
 </body>
 </html>
